08746de677c5ca6682f1814421c93126fc7486aa
kejsper\Programowanie <kejsper@gmail.com>
2017-06-21 01:03:02 +0200

    navigation 1.0 done

--- js/script.js
+++ js/script.js
@@ -3,40 +3,44 @@ $(document).ready(function() {
   var intro2 = $('.intro2');
   var slide = $('.slide');
   var slider = $('.slider');
+  var slide0height = $('#slide0').height();
+  var slide1height = $('#slide1').height();
+  var slide2height = $('#slide2').height();
+  var slide3height = $('#slide3').height();
   var next = $('.next');
   var back = $('.back');
   var howManySlides = slide.length + 1;
   whichSlide = 0;
 
   windowSize ();
-  /*overflowSlide();*/
   $(window).on('resize', function () {
     setTimeout(windowSize, 500);
   });
 
   function windowSize () {
     height = $(window).height();
-    /* if dla scrolli? */
-    width = $(window).width();
-
+    width = $(window).outerWidth(true);
     $('.wrapper').css('height', height).css('width', width);
     slider.css('height', height).css('width', width*howManySlides);
     intro1.css('height', height).css('width', width);
     $('.intro-wrapper').css('height', height).css('width', width);
-
     slide.css('height', height).css('width', width).css('left', (-width));
     $('.slide-main').css('height', height).css('width', width);
     if (whichSlide > 1) {
       slider.css('margin-left', -(width*(whichSlide-1)));
     }
-  }
-
-  function overflowSlide() {
-    if (whichSlide===0) {
-      $('.wrapper').addClass('overflow-y');
+    if (whichSlide!==0) {
+      if (slide1height>height) {
+        $('#slide1').addClass('overflow-y');
+      }
+      if (slide2height>height) {
+        $('#slide2').addClass('overflow-y');
+      }
+      if (slide3height>height) {
+        $('#slide3').addClass('overflow-y');
+      }
     }
   }
-
 
   next.on('click', function () {
     var margin = $(window).width();
@@ -46,16 +50,18 @@ $(document).ready(function() {
       });
 
       whichSlide = whichSlide+1;
+      windowSize();
       back.css('visibility', 'visible');
     }
     else if (whichSlide > 0 && whichSlide < howManySlides) {
-
       slider.animate({marginLeft: -(whichSlide * margin)});
       whichSlide = whichSlide + 1;
+      windowSize();
       if ((whichSlide + 1)===howManySlides){
         next.css('visibility', 'hidden');
       }
     }
+    editNav(whichSlide);
   });
 
   back.on('click', function () {
@@ -74,6 +80,21 @@ $(document).ready(function() {
         next.css('visibility', 'visible');
       }
     }
+    editNav(whichSlide);
   });
 
+  function editNav(whichSlide) {
+    switch (whichSlide) {
+      case 1:
+        $('#nav-back').html('powitanie');
+        $('#nav-next').html('moje prace');
+        break;
+      case 2:
+        $('#nav-back').html('umiejętności');
+        $('#nav-next').html('kontakt');
+        break;
+      case 3:
+        $('#nav-back').html('moje prace');
+    }
+  }
 });
